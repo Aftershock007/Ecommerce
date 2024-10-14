912d069ca2569eae74fa0e10f6550e36505b48ae
Aftershock007 <ab.19u10103@btech.nitdgp.ac.in>
2024-10-14 11:30:28 +0530

    json parsing middleware

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,12 @@
-import express from "express"
+import express, { json, urlencoded } from "express"
 import productRoutes from "./routes/products/index"
 
 const port = 3000
 const app = express()
+
+// middlewares: we can take json and urlencoded data by this
+app.use(json())
+app.use(urlencoded({ extended: false }))
 
 app.get("/", (req, res) => {
   res.send("Hello World!")
