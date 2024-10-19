3a72c61b9055fd672b7b69f278ea1c287824abd4
Aftershock007 <ab.19u10103@btech.nitdgp.ac.in>
2024-10-19 16:46:20 +0530

    product details screen

--- ecommerce-mobile/app/index.tsx
+++ ecommerce-mobile/app/index.tsx
@@ -1,18 +1,15 @@
 import { FlatList } from "react-native"
 import products from "../assets/products.json"
 import ProductListItem from "../components/ProductListItem"
-import { Button, ButtonText } from "@/components/ui/button"
 
 export default function HomeScreen() {
-  // return (
-  //   <FlatList
-  //     data={products}
-  //     renderItem={({ item }) => <ProductListItem product={item} />}
-  //   />
-  // )
   return (
-    <Button variant="outline">
-      <ButtonText>Press</ButtonText>
-    </Button>
+    <FlatList
+      data={products}
+      numColumns={2}
+      contentContainerClassName="gap-2"
+      columnWrapperClassName="gap-2"
+      renderItem={({ item }) => <ProductListItem product={item} />}
+    />
   )
 }
