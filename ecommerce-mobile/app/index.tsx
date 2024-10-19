b26b4174518cf6daaacffab4df2919c42f942c70
Aftershock007 <ab.19u10103@btech.nitdgp.ac.in>
2024-10-19 14:52:28 +0530

    gluestack ui setup

--- ecommerce-mobile/app/index.tsx
+++ ecommerce-mobile/app/index.tsx
@@ -1,12 +1,18 @@
 import { FlatList } from "react-native"
 import products from "../assets/products.json"
 import ProductListItem from "../components/ProductListItem"
+import { Button, ButtonText } from "@/components/ui/button"
 
 export default function HomeScreen() {
+  // return (
+  //   <FlatList
+  //     data={products}
+  //     renderItem={({ item }) => <ProductListItem product={item} />}
+  //   />
+  // )
   return (
-    <FlatList
-      data={products}
-      renderItem={({ item }) => <ProductListItem product={item} />}
-    />
+    <Button variant="outline">
+      <ButtonText>Press</ButtonText>
+    </Button>
   )
 }
